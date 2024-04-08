1bdd3c0518effce3c91bfb2250a83ae6c35c2036
JohnM89 <mjammer2@gmail.com>
2024-04-07 22:32:46 -0400

    updated vite hopefully to handle the bigger chache sizes

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,31 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Move all code from node_modules into a separate chunk
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   plugins: [
     react(),
     // Add the PWA plugin with some basic options
     VitePWA({
+  workbox: {
+    maximumFileSizeToCacheInBytes: 5 * 1024 * 1024, // Increase the limit as necessary, e.g., to 5MB
+  },
+  
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt'], // Additional assets to cache
       manifest: {
-        name: 'Your App Name',
-        short_name: 'AppShortName',
+        name: 'Pizza App',
+        short_name: 'PZA',
         theme_color: '#ffffff',
         icons: [
           {
@@ -29,5 +45,7 @@ export default defineConfig({
         ],
       },
     }),
+    
   ],
+  chunkSizeWarningLimit: 1000,
 });
